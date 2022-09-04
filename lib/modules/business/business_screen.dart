60ff7a07484b19eae08951158fc554c2521cced8
Eldeengawy <103843598+Eldeengawy@users.noreply.github.com>
2022-09-04 19:14:46 +0200

    complete Business Screen

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -1,16 +1,45 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/cubit.dart';
+import 'package:news_app/cubit/states.dart';
+import 'package:news_app/shared/components/components.dart';
 
 class BusinessScreen extends StatelessWidget {
   const BusinessScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Buisiness Screen",
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black)),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var list = NewsCubit.get(context).business;
+        return ConditionalBuilder(
+          condition: State is! NewsGetBusinessLoadingState,
+          builder: (context) => ListView.separated(
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) => buildArticleItem(list[index]),
+            separatorBuilder: (context, index) => myDivider(),
+            itemCount: 10,
+          ),
+          fallback: (context) => Center(
+            child: CircularProgressIndicator(
+              color: Colors.deepOrange,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget myDivider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        color: Colors.grey,
+        width: double.infinity,
+        height: 1.0,
+      ),
     );
   }
 }
---
+++ lib/shared/components/components.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+Widget buildArticleItem(articel) => Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
+            width: 120.0,
+            height: 120.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage(
+                  '${articel['urlToImage']}',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Expanded(
+            child: Container(
+              height: 120.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                      '${articel['title']}',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Text(
+                    '${articel['publishedAt']}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
