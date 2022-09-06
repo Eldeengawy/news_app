71395e476d9d60d3518aa090537e113cbe774e2d
Eldeengawy <103843598+Eldeengawy@users.noreply.github.com>
2022-09-06 19:46:54 +0200

    search

--- lib/layout/news_layout.dart
+++ lib/layout/news_layout.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/app_cubit/cubit.dart';
 import 'package:news_app/cubit/cubit.dart';
 import 'package:news_app/cubit/states.dart';
+import 'package:news_app/modules/search/search_screen.dart';
+import 'package:news_app/shared/components/components.dart';
 import 'package:news_app/shared/network/remote/dio_helper.dart';
 
 class NewsLayout extends StatelessWidget {
@@ -10,60 +12,59 @@ class NewsLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (BuildContext context) => NewsCubit()..getBusiness(),
-      child: BlocConsumer<NewsCubit, NewsStates>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          var cubit = NewsCubit.get(context);
-          return Scaffold(
-            // floatingActionButton: FloatingActionButton(
-            //   onPressed: () {
-            //     DioHelper.getData(
-            //       url: 'v2/top-headlines',
-            //       query: {
-            //         'country': 'eg',
-            //         'category': 'business',
-            //         'apiKey': 'b4bf465614544fe1841b1667074574ac',
-            //       },
-            //     ).then((value) {
-            //       print(value?.data.toString());
-            //     }).catchError((error) {
-            //       print(error.toString);
-            //     });
-            //   },
-            //   child: Icon(Icons.add),
-            // ),
-            appBar: AppBar(
-              title: const Text("News app"),
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.search,
-                  ),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var cubit = NewsCubit.get(context);
+        return Scaffold(
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {
+          //     DioHelper.getData(
+          //       url: 'v2/top-headlines',
+          //       query: {
+          //         'country': 'eg',
+          //         'category': 'business',
+          //         'apiKey': 'b4bf465614544fe1841b1667074574ac',
+          //       },
+          //     ).then((value) {
+          //       print(value?.data.toString());
+          //     }).catchError((error) {
+          //       print(error.toString);
+          //     });
+          //   },
+          //   child: Icon(Icons.add),
+          // ),
+          appBar: AppBar(
+            title: const Text("News app"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  navigateTo(context, SearchScreen());
+                },
+                icon: const Icon(
+                  Icons.search,
                 ),
-                IconButton(
-                  onPressed: () {
-                    AppCubit.get(context).changeMode();
-                  },
-                  icon: const Icon(
-                    Icons.brightness_4_sharp,
-                  ),
+              ),
+              IconButton(
+                onPressed: () {
+                  AppCubit.get(context).changeMode();
+                },
+                icon: const Icon(
+                  Icons.brightness_4_sharp,
                 ),
-              ],
-            ),
-            body: cubit.screens[cubit.currentIndex],
-            bottomNavigationBar: BottomNavigationBar(
-              currentIndex: cubit.currentIndex,
-              items: cubit.bottomItems,
-              onTap: (index) {
-                cubit.changeBottomNavBar(index);
-              },
-            ),
-          );
-        },
-      ),
+              ),
+            ],
+          ),
+          body: cubit.screens[cubit.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: cubit.currentIndex,
+            items: cubit.bottomItems,
+            onTap: (index) {
+              cubit.changeBottomNavBar(index);
+            },
+          ),
+        );
+      },
     );
   }
 }
