81f1b657fbdcd1a4bfa1b34367567afe5336976d
Eldeengawy <103843598+Eldeengawy@users.noreply.github.com>
2022-09-06 19:50:43 +0200

    testSearch

---
+++ lib/modules/search/test_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Search2 extends StatefulWidget {
+  const Search2({super.key});
+
+  @override
+  State<Search2> createState() => _Search2State();
+}
+
+class _Search2State extends State<Search2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -8,3 +8,4 @@
 //TODO api search 
 //https://newsapi.org/v2/everything?q=tesla&apiKey=b4bf465614544fe1841b1667074574ac
 //
+
